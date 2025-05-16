c129cddd1cd326a2e7c790dd0e08ae75b01cfa25
Andrewxa33 <131189314+Andrewxa33@users.noreply.github.com>
2025-05-16 16:53:23 -0500

    Commentarios

--- Sprint4/funciones.py
+++ Sprint4/funciones.py
@@ -4,6 +4,11 @@ pacientes = []
 tutores = []
 
 def registrar_mascota():
+    '''Resgitra una nueva mascota en el sistema junto con su tutor siguiendo el siguiente proceso:
+    Se solicitan los datos de la mascota (nombre, especie, raza)
+    Se solicitan los datos del tutor (nombre_tutor, telefono, direccion)
+    Se crean los objetos de las clases Dueno y Mascota
+    Se almacena la informacion en las listas tutores y pacientes'''
     print("\n--- Registrar Mascota ---")
     nombre = input("Nombre de la mascota: ")
     especie = input("Especie: ")
@@ -22,6 +27,13 @@ def registrar_mascota():
     print(f"\n✅ Mascota '{nombre}' registrada con éxito.")
 
 def registrar_consulta():
+    '''Metodo que registra una consulta asociada a una mascota existente.
+    El flujo es el siguiente:
+    Se solicita el nombre de la mascota.
+    Si la mascota existe, se procede a solicitar la informacion asociada a la consulta.
+    Se crea y almacena la consulta asociada a la mascota.
+    Si no existe, arroja una advertencia al usuario de que la mascota no existe
+    '''
     print("\n--- Registrar Consulta ---")
     nombre = input("Nombre de la mascota: ")
     mascota = buscar_mascota(nombre)
@@ -39,6 +51,10 @@ def registrar_consulta():
         print(f" Mascota '{nombre}' no encontrada.")
 
 def listar_mascotas():
+    '''Este metodo, muestra todas las mascotas registradas en el sistema.
+    La informacion que arroja cuando se ejecuta es la siguiente:
+    Los datos perteneciente a la mascota son: Nombre, especie, raza y datos del tutor.
+    En caso de no encontrar mascotas nos da un aviso'''
     print("\n--- Lista de Mascotas ---")
     if not pacientes:
         print("No hay mascotas registradas.")
@@ -47,6 +63,12 @@ def listar_mascotas():
             mascota.informacion()
 
 def ver_historial():
+    '''Este metodo, muestra el historial completo de las consultas asociadas a una mascota especifica.
+    El flujo es el siguiente:
+    Busca la mascota por el nombre.
+    Si existe, muestra todas las consultas asociadas a dicha mascota.
+    Usa el metodo mostrar_historial() de la clase Mascota
+    Imprime un aviso en caso de no encontrar a la mascota'''
     print("\n--- Historial de Consultas ---")
     nombre = input("Nombre de la mascota: ")
     mascota = buscar_mascota(nombre)
@@ -57,6 +79,9 @@ def ver_historial():
         print(f"❌ Mascota '{nombre}' no encontrada.")
 
 def buscar_mascota(nombre):
+    '''Este metodo, busca una mascota por su nombbre, es case sensitive, por ello se anade .lower()
+    para su ejecucion, debe proveerse el argumento tipo string nombre.
+    El retorno en caso de encontrarlo es el objeto mascota o None en caso de que no haya nada '''
     for m in pacientes:
         if m.nombre_paciente.lower() == nombre.lower():
             return m
