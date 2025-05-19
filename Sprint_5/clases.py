6a7879b6548fa1f129514fcf74450a847116e046
Camilo Vargas <camilo.vargas0419@hotmail.com>
2025-05-18 21:24:15 -0500

    Spring_5 - Con Excepciones

---
+++ Sprint_5/clases.py
@@ -0,0 +1,60 @@
+
+class Mascota:
+    '''La clase Mascota, representa a una mascota en el ejercicio de la clinica veterinaria.
+    Sus atributos son: Nombre paciente, especie, raza, todos con tipo de dato Str, tambien,
+    se añade a sus atributos un objeto dueno de la clase Dueno descrita en este mismo ejercico.
+    Todos los atributos se almacenan en la lista consultas[]
+    '''
+    def __init__(self, nombre_paciente, especie, raza, dueno):
+        '''El metodo constructor inicializa una nueva instancia de la clase Mascota con los argumentos
+        nombre_paciente, especie, raza, dueno'''
+        self.nombre_paciente = nombre_paciente
+        self.especie = especie
+        self.raza = raza
+        self.dueno = dueno
+        self.consultas = []
+
+    def informacion(self):
+        '''Este metodo muestra la informacion basica de la mascota y su dueño'''
+        print(f"Hola, me llamo {self.nombre_paciente}, soy un {self.especie}, de raza {self.raza} y mi tutor es {self.dueno.nombre_tutor}")
+
+    def mostrar_historial(self):
+        if not self.consultas:
+            print(f"No hay consultas registradas para {self.nombre_paciente}.")
+        else:
+            print(f"\nHistorial de consultas de {self.nombre_paciente}:")
+            for c in self.consultas:
+                print(f" - {c}")
+        '''Este metodo muestra la informacion relacionada con las consultas asociadas a la mascota'''
+
+class Dueno:
+    '''La clase Dueno, representa a un dueno en el ejercicio de la clinica veterinaria.
+    Sus atributos son: nombre_tutor, telefono, direccion, todos con tipo de dato Str.
+    '''
+    def __init__(self, nombre_tutor, telefono, direccion):
+        '''Metodo constructor que inicializa un objeto dueno de la clase Dueno con los argumentos:
+        nombre_tutor, telefono, direccion'''
+        self.nombre_tutor = nombre_tutor
+        self.telefono = telefono
+        self.direccion = direccion
+
+    def informacion(self):
+        print(f"Soy {self.nombre_tutor}, Tel: {self.telefono}, Dirección: {self.direccion}")
+        '''Metodo que muestra la informacion de un dueno de una mascota'''
+
+class Consulta:
+    '''La clase Consulta, representa una consulta en la veterinaria.
+    Sus atributos son: fecha, motivo, diagnostico, todos de tipo Str.'''
+
+    def __init__(self, fecha, motivo, diagnostico):
+        '''Metodo constructor que inicializa un objeto consulta de la clase Consulta con los argumentos:
+        fecha, motivo y diagnostico'''
+        self.fecha = fecha
+        self.motivo = motivo
+        self.diagnostico = diagnostico
+
+    def __str__(self):
+        '''Representación en string de la consulta
+        Returns:
+            str: Información formateada de la consulta'''
+        return f"{self.fecha} - Motivo: {self.motivo} | Diagnóstico: {self.diagnostico}"
---
+++ Sprint_5/funciones.py
@@ -0,0 +1,138 @@
+from clases import Mascota, Dueno, Consulta
+
+pacientes = []
+tutores = []
+
+def registrar_mascota():
+    '''Resgitra una nueva mascota en el sistema junto con su tutor siguiendo el siguiente proceso:
+    Se solicitan los datos de la mascota (nombre, especie, raza)
+    Se solicitan los datos del tutor (nombre_tutor, telefono, direccion)
+    Se crean los objetos de las clases Dueno y Mascota
+    Se almacena la informacion en las listas tutores y pacientes'''
+    try:
+        print("\n--- Registrar Mascota ---")
+        nombre = input("Nombre de la mascota: ").strip()
+        if not nombre:
+            raise ValueError("El nombre de la mascota no puede estar vacío")
+            #Valida que el nombre de la mascota no este en blanco.
+        especie = input("Especie: ").strip()
+        if not especie:
+            raise ValueError("La especie no puede estar vacía")
+            #Valida que la especie no este en blanco.
+        raza = input("Raza: ").strip()
+        if not raza:
+            raise ValueError("La raza no puede estar vacía")
+            #Valida que la raza no este vacia.
+
+        print("--- Datos del Tutor ---")
+
+        nombre_tutor = input("Nombre del tutor: ").strip()
+        if not nombre_tutor:
+            raise ValueError("El nombre del tutor no puede estar vacío")
+            #Valida que el nombre del tutor no este vacio.
+        telefono = input("Teléfono: ").strip()
+        if not telefono.isdigit() or len(telefono) != 10:
+            raise ValueError("El teléfono debe tener exactamente 10 dígitos numéricos.")
+            #Valida que el número telefónico solo sean numeros y sean 10 digitos exactamente.
+        direccion = input("Dirección: ").strip()
+        if not direccion:
+            raise ValueError("La direccion no puede estar vacía")
+            #Valida que la dirección no este vacia.
+
+        #if not all([nombre, especie, raza, nombre_tutor, telefono,direccion]):
+        #   raise ValueError("Todos los campos deben ser completados.")  Valida que todos los campos no estuvieran vacios
+        
+        dueno = Dueno(nombre_tutor, telefono, direccion)
+        mascota = Mascota(nombre, especie, raza, dueno)
+
+        tutores.append(dueno)
+        pacientes.append(mascota)
+
+        print(f"\nMascota '{nombre}' registrada con éxito.")
+
+    except ValueError as ve:
+        print(f" Error: {ve}")
+    except Exception as e:
+        print(" Ocurrió un error inesperado.")
+
+def registrar_consulta():
+    '''Metodo que registra una consulta asociada a una mascota existente.
+    El flujo es el siguiente:
+    Se solicita el nombre de la mascota.
+    Si la mascota existe, se procede a solicitar la informacion asociada a la consulta.
+    Se crea y almacena la consulta asociada a la mascota.
+    Si no existe, arroja una advertencia al usuario de que la mascota no existe
+    '''
+    print("\n--- Registrar Consulta ---")
+    try:
+        nombre = input("Nombre de la mascota: ")
+        mascota = buscar_mascota(nombre)
+
+        if  not mascota:
+            raise LookupError(f"Mascota '{nombre}' no encontrada.")
+            #Verifica si la mascota existe.
+
+        fecha = input("Fecha de la consulta (dd/mm/aaaa): ").strip()
+        motivo = input("Motivo: ").strip()
+        diagnostico = input("Diagnóstico: ").strip()
+        #Inputs de entrada
+
+        if not all([fecha, motivo, diagnostico]):
+            raise ValueError("Todos los campos de la consulta deben estar completos.")
+            #Verifica el error si algun campo esta vacío.
+
+        nueva_consulta = Consulta(fecha, motivo, diagnostico)
+        mascota.consultas.append(nueva_consulta)
+
+        print(f"\n Consulta registrada para {mascota.nombre_paciente}.")
+    except LookupError as le:
+        print(f"{le}")
+    except ValueError as ve:
+        print(f"Error: {ve}")
+    except Exception as e:
+        print("Ocurrió un error inesperado.")
+
+def listar_mascotas():
+    '''Este metodo, muestra todas las mascotas registradas en el sistema.
+    La informacion que arroja cuando se ejecuta es la siguiente:
+    Los datos perteneciente a la mascota son: Nombre, especie, raza y datos del tutor.
+    En caso de no encontrar mascotas nos da un aviso'''
+    print("\n--- Lista de Mascotas ---")
+    if not pacientes:
+        print("No hay mascotas registradas.")
+    else:
+        for mascota in pacientes:
+            mascota.informacion()
+
+def ver_historial():
+    '''Este metodo, muestra el historial completo de las consultas asociadas a una mascota especifica.
+    El flujo es el siguiente:
+    Busca la mascota por el nombre.
+    Si existe, muestra todas las consultas asociadas a dicha mascota.
+    Usa el metodo mostrar_historial() de la clase Mascota
+    Imprime un aviso en caso de no encontrar a la mascota'''
+
+    print("\n--- Historial de Consultas ---")
+    try:
+        nombre = input("Nombre de la mascota: ")
+        mascota = buscar_mascota(nombre)
+
+        if mascota:
+            mascota.mostrar_historial()
+        else:
+            raise LookupError(f"Mascota '{nombre}' no encontrada.")
+        
+    except LookupError as le:
+        print(le)
+
+    except Exception as e:
+        print("Ocurrio un error inesperado al mostrar el historial")
+
+def buscar_mascota(nombre):
+    '''Este metodo, busca una mascota por su nombbre, es case sensitive, por ello se anade .lower()
+    para su ejecucion, debe proveerse el argumento tipo string nombre.
+    El retorno en caso de encontrarlo es el objeto mascota o None en caso de que no haya nada '''
+    for m in pacientes:
+        if m.nombre_paciente.lower() == nombre.lower():
+            return m
+    return None
