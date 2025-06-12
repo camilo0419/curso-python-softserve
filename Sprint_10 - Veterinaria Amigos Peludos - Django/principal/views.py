9ce52138ccde3cb44e00b1066d1150dacd8eb391
Camilo Vargas <camilo.vargas0419@hotmail.com>
2025-06-11 22:24:05 -0500

    Tabla de consultas por fecha en HTML/inicio

--- Sprint_10 - Veterinaria Amigos Peludos - Django/principal/views.py
+++ Sprint_10 - Veterinaria Amigos Peludos - Django/principal/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from conexion import crear_conexion  
+from datetime import date
 
 def inicio(request):
-    return render(request, 'principal/inicio.html')
+    fecha = request.GET.get('fecha', date.today().isoformat())
+    conexion = crear_conexion()
+    cursor = conexion.cursor()
+    cursor.execute("""
+        SELECT consultas.id, consultas.motivo, consultas.diagnostico, mascotas.nombre_mascota
+        FROM consultas
+        JOIN mascotas ON consultas.mascota_id = mascotas.id
+        WHERE consultas.fecha = ?
+    """, (fecha,))
+    consultas = cursor.fetchall()
+    conexion.close()
+
+    return render(request, 'principal/inicio.html', {
+        'consultas': consultas,
+        'fecha': fecha
+    })
 
 def clientes(request):
     return render(request, 'principal/clientes.html')
